511d615e1250f904c690e1f8e789a9526b8f1d9c
Carlos <carlosaugusto19991@poli.ufrj.br>
2020-11-17 12:06:02 -0300

    adding login page

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Login from '../pages/Login';
 
 export default function App() {
     return (
-        <>organizing files</>
+        <Router>
+            <Switch>
+                <Route path='/' component={Login} />
+            </Switch>
+        </Router>
     );
 }
---
+++ src/styles/styledLogin.js
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+
+export const LoginContainer = styled.div`
+    display: flex;
+    height: 80vh;
+    justify-content: center;
+    align-items: center;
+
+    img {
+        width: 40vw;
+        height: 50vh;
+    }
+`;
+
+export const FormContainer = styled.form`
+    width: 40vw;
+    height: 50vh;
+    background-color: #3888c2;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    h1, a {
+        font-family: 'Righteous', cursive;
+        color: #fae52a;
+    }
+
+    h1 {
+        font-size: 22px;
+        margin-top: -20;
+        padding-bottom: 20px;
+    }
+
+    a {
+        padding-top: 10px;
+        cursor: pointer;
+    }
+
+    input, button {
+        width: 90%;
+        border: none;
+        border-radius: 3px;
+        padding: 13px 5px;
+        outline-style: none;
+        background-color: #fae52a;
+        color: #3888c2;
+        margin: 5px 0;
+        font-size: 16px;
+        font-weight: bold;
+    }
+
+    input {
+        text-align: center;
+        
+        ::placeholder {
+            color: #7ba9d1;
+        }
+    }
+
+    button {
+        margin-top: 20px;
+        cursor: pointer;
+    }
+
+    button:hover {
+        background-color: #dbd437;
+    }
+`;
