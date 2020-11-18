af70f24a21731ea1b6ec43a6b3c118bd019802e9
Carlos <carlosaugusto19991@poli.ufrj.br>
2020-11-18 12:25:07 -0300

    adding function to add and remove quantity of toys

--- src/components/ShopList.js
+++ src/components/ShopList.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { IoIosAddCircle, IoIosRemoveCircle} from 'react-icons/io'
 
 export default function ShopList() {
+
+    const [quantity, setQuantity] = useState(0);
+
+    function removeQuantity() {
+        if(quantity >= 1) setQuantity(quantity - 1);
+    }
 
     return (
         <ShopContainer>
             <span>
                 <img src='https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcShI62H_s2tVghBMOZxDXmi1hqsdxUzI1MkDYEDH-hDJClYigUSf1hpLOMOzOGQyrQU2oQSLp0uZh9p9nszwPlJCmSZnN6VgZIOCZreOz0FdoAddup-gFXGHQ&usqp=CAE' />
-                <strong>Minion Stuart</strong>
+                <div><strong>Minion Stuart</strong><strong>R$ 29,99</strong></div>
                 <p>Boneco Minions Stuart Filme Tamanho Pequeno</p>
-                <strong>R$ 29,99</strong>
+                <div>
+                    <IoIosAddCircle onClick={() => setQuantity(quantity + 1)} />
+                    <p>{quantity > 0 ? quantity : ''}</p>
+                    <IoIosRemoveCircle  className='remove' onClick={removeQuantity} />
+                </div>
             </span>
             <span>
                 <img src='https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcShI62H_s2tVghBMOZxDXmi1hqsdxUzI1MkDYEDH-hDJClYigUSf1hpLOMOzOGQyrQU2oQSLp0uZh9p9nszwPlJCmSZnN6VgZIOCZreOz0FdoAddup-gFXGHQ&usqp=CAE' />
@@ -56,6 +67,26 @@ const ShopContainer = styled.main`
             padding: 5px;
             color: #3888c2;
             font-size: 12px;
+        }
+
+        div {
+            display: flex;
+            justify-content: space-between;
+
+            p {
+                margin-top: 6px;
+            }
+        }
+
+        svg {
+            padding: 5px;
+            font-size: 35px;
+            color: #3888c2;
+            cursor: pointer;
+
+            &.remove {
+                color: #d1c73f;
+            }
         }
     }
 `;
